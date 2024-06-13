1f1456a4455fc938be70b0204d49550ee47f3cd6
normalid <rluo2018@gmail.com>
2024-06-13 17:15:31 +0800

    Android and PWA support

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'ipsc-smart-system-ionic',
-  webDir: 'dist'
+	appId: "io.ionic.starter",
+	appName: "ipsc-smart-system-ionic",
+	webDir: "dist",
 };
 
 export default config;
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import { setupIonicReact } from "@ionic/react";
+import { defineCustomElements } from "@ionic/pwa-elements/loader";
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
+defineCustomElements(window);
+
 
 setupIonicReact({
 	rippleEffect: false,
