0cee8a53638cb2515f6b5d76c2afea07504b2c1e
normalid <rluo2018@gmail.com>
2024-06-13 15:24:29 +0800

    setup editor config

--- src/App.tsx
+++ src/App.tsx
@@ -38,21 +38,21 @@ setupIonicReact();
 
 const App: React.FC = () => {
   return (
-    <IonApp>
-      <IonReactRouter>
-        <IonSplitPane contentId="main">
-          <Menu />
-          <IonRouterOutlet id="main">
-            <Route path="/" exact={true}>
-              <Redirect to="/folder/Inbox" />
-            </Route>
-            <Route path="/folder/:name" exact={true}>
-              <Page />
-            </Route>
-          </IonRouterOutlet>
-        </IonSplitPane>
-      </IonReactRouter>
-    </IonApp>
+	<IonApp>
+	  <IonReactRouter>
+		<IonSplitPane contentId="main">
+		  <Menu />
+		  <IonRouterOutlet id="main">
+			<Route path="/" exact={true}>
+				<Redirect to="/folder/Inbox" />
+			</Route>
+			<Route path="/folder/:name" exact={true}>
+				<Page />
+			</Route>
+		  </IonRouterOutlet>
+		</IonSplitPane>
+	  </IonReactRouter>
+	</IonApp>
   );
 };
 
