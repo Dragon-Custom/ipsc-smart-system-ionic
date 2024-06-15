4258906e9194d7c1284173e83645dca7e4f1650d
normalid <rluo2018@gmail.com>
2024-06-15 19:39:11 +0800

    update use control hook

--- src/lib/useControl.tsx
+++ src/lib/useControl.tsx
@@ -2,14 +2,16 @@ import { useState } from "react";
 
 export function useControl<T>(
 	defaultValue: T,
-	elements: (value: T, onChange: (value: T) => void) => React.ReactElement
+	elements: (value: T, onChange: (value: T) => void) => React.ReactElement,
+	onChange: (newValue: T, previousValue: T, setValue: (value: T) => void) => void,
 ): [[T, (value: T) => void], React.ReactElement] {
 	const [value, setValue] = useState(defaultValue);
-	const onChange = (newValue: T) => {
+	const change = (newValue: T) => {
+		onChange(newValue, value, setValue);
 		setValue(newValue);
 	};
 
-	const element: React.ReactElement = <>{elements(value, onChange)}</>;
+	const element: React.ReactElement = <>{elements(value, change)}</>;
 
 	return [[value, setValue], element];
 }
