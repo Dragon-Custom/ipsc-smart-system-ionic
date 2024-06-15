61f94768933a9d584bd82f4f783a16a89636ad68
normalid <rluo2018@gmail.com>
2024-06-15 16:09:42 +0800

    Config the manifest

--- src/main.tsx
+++ src/main.tsx
@@ -6,13 +6,35 @@ import { defineCustomElements } from "@ionic/pwa-elements/loader";
 import "./theme/variables.css";
 import "./index.css";
 import { App as KUIApp } from "konsta/react";
+import { KonstaProvider } from "konsta/react";
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
 
 defineCustomElements(window);
 
-import { KonstaProvider } from "konsta/react";
+window.addEventListener("beforeinstallprompt", (e) => {
+	console.log("beforeinstallprompt Event fired");
+	// Prevent Chrome 67 and earlier from automatically showing the prompt
+	// e.preventDefault();
+	// Stash the event so it can be triggered later.
+	let deferredPrompt = e;
+	let showInstallBtn = true;
+	if (deferredPrompt !== undefined && deferredPrompt !== null) {
+		// Show the prompt
+		deferredPrompt.prompt();
+		// Wait for the user to respond to the prompt
+		deferredPrompt.userChoice.then((choiceResult) => {
+			if (choiceResult.outcome === "accepted") {
+				console.log("User accepted the A2HS prompt");
+			} else {
+				console.log("User dismissed the A2HS prompt");
+			}
+			// We no longer need the prompt.  Clear it up.
+			deferredPrompt = null;
+		});
+	}
+});
 
 setupIonicReact({
 	rippleEffect: true,
